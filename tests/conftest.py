f1c23687bbe74ca2cee0a73661f854c1c20b07ce
liblaf <30631553+liblaf@users.noreply.github.com>
2025-12-03 01:22:52 +0800

    test(config): Enable JAX tracer leak checking
    This change updates the pytest configuration in `conftest.py` to enable `jax_check_tracer_leaks`. This is necessary to help catch potential JAX tracer leaks during test runs, which can indicate issues with JAX transformations and lead to incorrect or unexpected behavior.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import pytest
 
 def pytest_configure(config: pytest.Config) -> None:  # noqa: ARG001
     hypothesis.settings.register_profile("default", max_examples=10, deadline=None)
+    jax.config.update("jax_check_tracer_leaks", True)  # noqa: FBT003
     jax.config.update("jax_debug_nans", True)  # noqa: FBT003
     jax.config.update("jax_default_matmul_precision", "highest")
     jax.config.update("jax_enable_x64", True)  # noqa: FBT003
